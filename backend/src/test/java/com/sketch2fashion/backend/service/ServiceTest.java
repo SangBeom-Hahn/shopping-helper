6b453f71065500a730cb276caad6d6bfc1b2aea7
SangBeom-Hahn <hsb990917@gmail.com>
2024-09-09 14:33:52 +0900

    feat: ClothesService 기능 및 테스트 구현
    #7

---
+++ backend/src/test/java/com/sketch2fashion/backend/service/ServiceTest.java
@@ -0,0 +1,18 @@
+package com.sketch2fashion.backend.service;
+
+import com.sketch2fashion.backend.repository.ClothesRepository;
+import com.sketch2fashion.backend.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SpringBootTest
+public abstract class ServiceTest {
+
+    @Autowired
+    protected ClothesService clothesService;
+
+    @Autowired
+    protected MessageRepository messageRepository;
+}
