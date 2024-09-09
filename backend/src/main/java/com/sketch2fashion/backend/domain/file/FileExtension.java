9e881b8a6ddaa05ef577f117360d132fd5ad6bc9
SangBeom-Hahn <hsb990917@gmail.com>
2024-09-09 17:04:45 +0900

    feat: FileExtension 기능 및 테스트 구현
    #7

---
+++ backend/src/main/java/com/sketch2fashion/backend/domain/file/FileExtension.java
@@ -0,0 +1,38 @@
+package com.sketch2fashion.backend.domain.file;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+@Getter
+public enum FileExtension {
+    JPEG(".jpeg"),
+    JPG(".jpg"),
+    PNG(".png"),
+    HEIC(".heic")
+    ;
+
+    private final String values;
+
+    FileExtension(String values) {
+        this.values = values;
+    }
+
+    public static FileExtension from(String fileName) {
+        return Arrays.stream(values())
+                .filter(fileExtension -> fileExtension.values.equals(getExtension(fileName)))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException());
+    }
+
+    private static String getExtension(String fileName) {
+        return fileName.substring(getDotIndex(fileName));
+    }
+
+    private static int getDotIndex(String fileName) {
+        return fileName.lastIndexOf(".");
+    }
+}
