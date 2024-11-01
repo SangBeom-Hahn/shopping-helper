4a7fda1d4678041db1d6c4c069e0c50a597a141e
SangBeom-Hahn <hsb990917@gmail.com>
2024-11-01 22:37:26 +0900

    test: MessageService Queue Publisher 목킹
    #5

--- backend/src/test/java/com/sketch2fashion/backend/service/MessageServiceTest.java
+++ backend/src/test/java/com/sketch2fashion/backend/service/MessageServiceTest.java
@@ -2,15 +2,23 @@ package com.sketch2fashion.backend.service;
 
 import com.sketch2fashion.backend.domain.message.ObjectType;
 import com.sketch2fashion.backend.service.dto.MessageResponseDto;
+import com.sketch2fashion.backend.support.MessagePublisher;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 
 class MessageServiceTest extends ServiceTest {
+
+    @MockBean
+    private MessagePublisher fakePublisher;
 
     @Test
     @DisplayName("존재하지 않는 메세지를 조회하면 예외가 발생한다.")
@@ -30,12 +38,13 @@ class MessageServiceTest extends ServiceTest {
         String storeFilePath = "path";
     
         // when
+        doNothing().when(fakePublisher).sendModelMessage(any());
         Long saveId = messageService.createMessage(storeFilePath)
                 .getId();
         MessageResponseDto messageResponseDto = messageService.findMessage(saveId);
 
         // then
         assertThat(messageResponseDto).extracting("id", "objectType", "storeFilePath")
-                .containsExactly(saveId, ObjectType.T_SHIRTS, "path");
+                .containsExactly(saveId, ObjectType.SKIRT, storeFilePath);
     }
 }
