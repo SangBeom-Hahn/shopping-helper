e8bf5c9cbb1e42e432d1bafe2a64287c82c8e348
SangBeom-Hahn <hsb990917@gmail.com>
2024-12-17 21:03:48 +0900

    feat: base_model_launcher 클라우드 SQL 도입
    #8

--- ml/model_launcher/base_model_launcher.py
+++ ml/model_launcher/base_model_launcher.py
@@ -9,23 +9,23 @@ from PIL import Image
 class Base_GAN():
     
     def __init__(self) -> None:
-        self.model = self._load_model()
+        self.storage_handler = StorageHandler()
     
     def _load_model(self):
         raise NotImplementedError(GAN_IMPLEMENTS_EXCEPTION_MESSAGE)
 
-    def inference(self, store_file_path: str) -> str:
+    def inference(self, store_file_path: str, message_id: int) -> str:
         img = self._preprocess(store_file_path)
         color_img = self.model.predict(img)
-        return self._postprocess(color_img)
+        return self._postprocess(color_img, store_file_path, message_id)
 
     def _preprocess(self, store_file_path):
-        img = self._load_img(store_file_path)
+        self.storage_handler.download_img(store_file_path)
+        img = self._load_img(os.path.join(UPLOAD_RELATIVE_PATH, store_file_path))
         return self._pre_normalize(img)
-        
-    def _load_img(self, store_file_path): # "../Untitled.png"
+    
+    def _load_img(self, store_file_path):
         self._validate_file_exists(store_file_path)
-        
         return load_img(store_file_path, target_size=(RESIZE_VALUE, RESIZE_VALUE))
 
     def _validate_file_exists(self, store_file_path):
@@ -34,19 +34,22 @@ class Base_GAN():
 
     def _pre_normalize(self, img):
         img = np.array(img)
-        img = (img - Normalize.SYMMETRIC_VALUE) / Normalize.SYMMETRIC_VALUE
+        img = (img - Normalize.SYMMETRIC_VALUE.value) / Normalize.SYMMETRIC_VALUE.value
         return img[np.newaxis, ...]
 
-    def _postprocess(self, color_img):
+    def _postprocess(self, color_img, store_file_path, message_id):
         color_img = self._normalize(color_img)
-        return self._save_process(color_img)
+        colored_file_path = self._save_process(color_img, store_file_path)
+        self.storage_handler.upload_img(colored_file_path, store_file_path)
+        self.storage_handler.changeClothesResultStatus(store_file_path, InferenceStatus.FINISH.name, InferenceStatus.FINISH.value, message_id)        
+        return colored_file_path
 
     def _normalize(self, image: np.ndarray) -> np.ndarray:
-        color_img = (image + 1) / 2.0 # 정규화
-        return (color_img[ZERO_IDX] * Normalize.MIN_MAX_VALUE).astype(np.uint8) # 0-255 범위로 변환
-
-    def _save_process(self, image):
-        save_path = "./result.png"
+        color_img = (image + 1) / 2.0
+        return (color_img[ZERO_IDX] * Normalize.MIN_MAX_VALUE.value).astype(np.uint8)
+    
+    def _save_process(self, image, store_file_path):
+        colored_file_path = os.path.join(RESULT_RELATIVE_PATH, store_file_path)
         color_img = Image.fromarray(image)
-        color_img.save(save_path)
-        return save_path
+        color_img.save(colored_file_path)
+        return colored_file_path
