a306e03af950b31c27e0cb1e6c638a325a490b91
SangBeom-Hahn <hsb990917@gmail.com>
2024-12-18 15:44:17 +0900

    feat: ResultRepository 평점 기능 및 테스트 구현
    #8

--- backend/src/test/java/com/sketch2fashion/backend/repository/ResultRepositoryTest.java
+++ backend/src/test/java/com/sketch2fashion/backend/repository/ResultRepositoryTest.java
@@ -3,25 +3,39 @@ package com.sketch2fashion.backend.repository;
 import com.sketch2fashion.backend.domain.message.Message;
 import com.sketch2fashion.backend.domain.message.ObjectType;
 import com.sketch2fashion.backend.domain.modelresult.ClothesResult;
+import com.sketch2fashion.backend.domain.modelresult.Rating;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.sketch2fashion.backend.domain.modelresult.Rating.FIRST;
+import static com.sketch2fashion.backend.domain.modelresult.Rating.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 class ResultRepositoryTest extends RepositoryTest {
 
     private Message message;
+    private Message message1;
+    private Message message2;
     private ClothesResult clothes;
+    private ClothesResult clothes1;
+    private ClothesResult clothes2;
 
     @BeforeEach
     void setUp() {
         message = new Message(ObjectType.T_SHIRTS, "path", false);
+        message1 = new Message(ObjectType.T_SHIRTS, "path", false);
+        message2 = new Message(ObjectType.T_SHIRTS, "path", false);
         clothes = new ClothesResult(message);
+        clothes1 = new ClothesResult(message1);
+        clothes2 = new ClothesResult(message2);
 
         messageRepository.save(message);
+        messageRepository.save(message1);
+        messageRepository.save(message2);
     }
 
     @Test
@@ -30,17 +44,49 @@ class ResultRepositoryTest extends RepositoryTest {
         // given
         Long saveId = resultRepository.save(clothes)
                 .getId();
+        int expectedRate = FIRST.getValue();
+        String expectedReview = "GOOD";
+        Boolean expectedShared = true;
 
         // when
         ClothesResult findClothes = resultRepository.findById(saveId)
                 .orElseThrow();
-        findClothes.changeRate(FIRST);
-        findClothes.changeShared(true);
+        findClothes.changeRate(expectedRate);
+        findClothes.changeShared(expectedShared);
+        findClothes.changeReview(expectedReview);
 
         // then
         Assertions.assertAll(
-                () -> assertThat(findClothes.getShared()).isEqualTo(true),
-                () -> assertThat(findClothes.getRating()).isEqualTo(FIRST)
+                () -> assertThat(findClothes.getShared()).isEqualTo(expectedShared),
+                () -> assertThat(findClothes.getRating()).isEqualTo(expectedRate),
+                () -> assertThat(findClothes.getReview()).isEqualTo(expectedReview)
         );
+    }
+
+    @Test
+    @DisplayName("전체 검색 개수를 구한다.")
+    void count() {
+        // given
+        resultRepository.save(clothes);
+        resultRepository.save(clothes1);
+        resultRepository.save(clothes2);
+
+        // then
+        assertThat(resultRepository.count()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("평점 평균을 계산한다.")
+    void averageOfRating() {
+        // given
+        clothes.changeRate(FIRST.getValue());
+        clothes1.changeRate(SECOND.getValue());
+        clothes2.changeRate(THIRD.getValue());
+        resultRepository.save(clothes);
+        resultRepository.save(clothes1);
+        resultRepository.save(clothes2);
+
+        // then
+        assertThat(resultRepository.averageOfRating()).isEqualTo(4L);
     }
 }
